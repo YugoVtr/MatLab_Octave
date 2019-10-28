db94f892753b212d93d4dd1651ca985ac8fb6897
PUC@ECEC <vtrhg69@hotmail.com>
2019-10-28 11:52:25 -0300

    aplicacao de mascara

---
+++ pdi/mascara.m
@@ -0,0 +1,12 @@
+a = imread('lena.jpg');
+b = rgb2gray(a);
+tamanho = 3;
+mask = ones(tamanho);
+fator = 1/tamanho*tamanho;
+[w, h] = size(b);
+for i = 1:w
+    for j = 1:h
+        
+    end
+end
+
